66c1c8fc0228de996dd0450c38dbe4bb5e4d9702
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-27 19:17:19 +0100

    add HW

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from .forms import *
+
+
 
 # Create your views here.
+
+def sayHello(request):
+ return HttpResponse('Hello World')
