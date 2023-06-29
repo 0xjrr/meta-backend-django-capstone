7e5268474bc59499044cf2ca61daec0bd9e3eec8
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-29 21:46:46 +0100

    add menu and booking routes

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from .views import *
+from rest_framework.routers import DefaultRouter
 
 
+router = DefaultRouter()
+router.register(r'menu', MenuItemsView, basename='menu')
+router.register(r'booking', BookingView, basename='booking')
 
 urlpatterns = [
     path('', index, name='home'),
+    path('', include(router.urls)),
 ]
