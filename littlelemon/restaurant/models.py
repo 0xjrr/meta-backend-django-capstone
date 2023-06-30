ae673de3e051efa884ac551716661bcd3697b7d1
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-30 01:20:49 +0100

    fix float field

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -4,8 +4,11 @@ from django.db import models
 
 class Menu(models.Model):
     tittle = models.CharField(max_length=255, null=False)
-    price = models.FloatField(null=False)
+    price = models.DecimalField(null=False, max_digits=6, decimal_places=2)
     iventory = models.IntegerField(default=0)
+
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
 
     def __str__(self):
         return self.tittle
