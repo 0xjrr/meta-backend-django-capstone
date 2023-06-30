d5fc8ae3958326a9f23698055821ef2c377fc3b4
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-30 04:06:20 +0100

    add

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import *
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 router = DefaultRouter()
@@ -9,5 +10,6 @@ router.register(r'booking', BookingView, basename='booking')
 
 urlpatterns = [
     path('', index, name='home'),
+    path('api-token-auth/', obtain_auth_token),
     path('', include(router.urls)),
 ]
