06878cd6324270bf798a032acc88411217bce365
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-30 11:17:01 +0100

    fix names

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Menu(models.Model):
-    tittle = models.CharField(max_length=255, null=False)
+    title = models.CharField(max_length=255, null=False)
     price = models.DecimalField(null=False, max_digits=6, decimal_places=2)
-    iventory = models.IntegerField(default=0)
+    inventory = models.IntegerField(default=0)
 
     def __str__(self):
         return f'{self.title} : {str(self.price)}'
