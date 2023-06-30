4d9eba47eaec28ee32032cefbecb893bc080e9c4
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-30 09:09:44 +0100

    change the str method

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -7,11 +7,9 @@ class Menu(models.Model):
     price = models.DecimalField(null=False, max_digits=6, decimal_places=2)
     iventory = models.IntegerField(default=0)
 
-    def get_item(self):
+    def __str__(self):
         return f'{self.title} : {str(self.price)}'
 
-    def __str__(self):
-        return self.tittle
 
 class Booking(models.Model):
     name = models.CharField(max_length=255)
